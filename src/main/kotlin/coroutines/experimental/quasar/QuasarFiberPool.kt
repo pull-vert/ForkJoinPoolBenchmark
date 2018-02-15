ce949df8a2d03594ca7d19f0b7645df0d1254434
frederic.montariol <frederic.montariol@adventiel.fr>
2018-02-15 17:42:17 +0100

    todos

--- src/main/kotlin/coroutines/experimental/quasar/QuasarFiberPool.kt
+++ src/main/kotlin/coroutines/experimental/quasar/QuasarFiberPool.kt
@@ -3,11 +3,15 @@ package coroutines.experimental.quasar
 import co.paralleluniverse.fibers.Fiber
 import co.paralleluniverse.fibers.FiberForkJoinScheduler
 import co.paralleluniverse.fibers.FiberScheduler
+import co.paralleluniverse.fibers.FiberUtil
 import kotlin.coroutines.experimental.AbstractCoroutineContextElement
 import kotlin.coroutines.experimental.Continuation
 import kotlin.coroutines.experimental.ContinuationInterceptor
 
 val fiberSchedulerName = "Fiber Scheduler CoroutineContext"
+
+// read http://docs.paralleluniverse.co/quasar/
+// todo add javaAgent ! remove scheduler and use Strand mais pas FiberUtil.runInFiber (car attend)
 
 fun fiberScheduler(parrallel: Int) = Pool(FiberForkJoinScheduler(fiberSchedulerName, 100))
 
@@ -30,6 +34,7 @@ private class PoolContinuation<T>(
     }
 
     override fun resumeWithException(exception: Throwable) {
+        FiberUtil.runInFiber {  }
         Fiber<Unit>(scheduler) {
             continuation.resumeWithException(exception)
         }.start()
